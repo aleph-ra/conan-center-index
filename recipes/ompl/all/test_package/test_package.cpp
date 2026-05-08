d28b6cf6122afa6721780111f94e264e7ceb0a3c
Uilian Ries <uilianr@jfrog.com>
2026-05-08 09:16:49 +0200

    Make test package consume boost and eigen as well
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/ompl/all/test_package/test_package.cpp
+++ recipes/ompl/all/test_package/test_package.cpp
@@ -1,16 +1,16 @@
-#include <ompl/base/SpaceInformation.h>
-#include <ompl/base/spaces/RealVectorStateSpace.h>
 #include <iostream>
+#include <memory>
+
+#include <ompl/base/SpaceInformation.h>
+#include <ompl/base/spaces/special/SphereStateSpace.h>
 
 int main() {
-    // Construct a simple 3D state space
-    auto space = std::make_shared<ompl::base::RealVectorStateSpace>(3);
-
-    // Construct a space information instance for this state space
+    auto space = std::make_shared<ompl::base::SphereStateSpace>(1.0);
     ompl::base::SpaceInformation si(space);
+    si.setStateValidityChecker([](const ompl::base::State *) { return true; });
+    si.setup();
 
-    // Simple check to ensure library loaded and objects can be created
-    std::cout << "OMPL Test: State space dimension is " << space->getDimension() << std::endl;
-
+    std::cout << "ompl test_package: space dim=" << space->getDimension()
+              << " state dim=" << si.getStateDimension() << std::endl;
     return 0;
 }
